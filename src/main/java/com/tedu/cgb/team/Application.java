ac60325ecb4ae9138dd054adfea0efe847463e58
yourname <418739318@qq.com>
2019-09-18 15:37:15 +0800

    test

--- src/main/java/com/tedu/cgb/team/Application.java
+++ src/main/java/com/tedu/cgb/team/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
-
+// test
 @EnableAsync // 启用Spring的异步配置
 @EnableCaching // 启用Spring中的Cache配置
 @SpringBootApplication
